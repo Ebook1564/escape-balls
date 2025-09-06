3cc90003bb2e00794091a033ab7395bb10afc930
Ebook1564 <myebookbundle@gmail.com>
2025-09-06 15:25:16 +0530

    level 1 changes

--- Assets/Peak/Scripts/DestroyOnCollision.cs
+++ Assets/Peak/Scripts/DestroyOnCollision.cs
@@ -7,9 +7,10 @@ public class DestroyOnCollision : MonoBehaviour
     public GameObject obstcle;
     private void Update()
     {
-        if(obstcle.transform.position.y<=-20)
+        if (obstcle != null && obstcle.transform.position.y <= -20)
         {
             Destroy(obstcle);
+            obstcle = null; // Optional: remove reference to avoid future access
         }
     }
     private void OnCollisionEnter2D(Collision2D collision)
